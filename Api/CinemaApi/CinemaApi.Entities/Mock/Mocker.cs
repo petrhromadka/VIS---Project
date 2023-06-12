1f6b30c3c0595c30e30501aee9b23bc531ea9913
Tobiáš Karpeta <tobiaskarpeta@hotmail.com>
2023-06-12 19:08:14 +0200

    Added movie descriptions

--- Api/CinemaApi/CinemaApi.Entities/Mock/Mocker.cs
+++ Api/CinemaApi/CinemaApi.Entities/Mock/Mocker.cs
@@ -34,7 +34,8 @@ namespace CinemaApi.Entities.Mock
                         new() { DateTime = new DateTime(2023, 3, 15, 14, 20, 0), Price = 180, Hall = hallOne },
                         new() { DateTime = new DateTime(2023, 3, 15, 17, 10, 0), Price = 180, Hall = hallOne },
                         new() { DateTime = new DateTime(2023, 3, 15, 21, 40, 0), Price = 180, Hall = hallOne },
-                    }
+                    },
+                    Description = "Scott Lang (Paul Rudd) a Hope Van Dyne (Evangeline Lilly) spolu s Hankem Pymem (Michael Douglas) a Janet Van Dyne (Michelle Pfeiffer) prozkoumávají Quantum Realm, kde interagují s podivnými tvory a vydávají se na dobrodružství, které přesahuje hranice toho, co považovali za možné."
                 },
                 new()
                 {
@@ -50,7 +51,8 @@ namespace CinemaApi.Entities.Mock
                         new() { DateTime = new DateTime(2023, 3, 13, 15, 10, 0), Price = 160, Hall = hallTwo},
                         new() { DateTime = new DateTime(2023, 3, 14, 15, 10, 0), Price = 160, Hall = hallTwo},
                         new() { DateTime = new DateTime(2023, 3, 15, 15, 10, 0), Price = 160, Hall = hallTwo},
-                    }
+                    },
+                    Description = "Píše se rok 50 př. n. l. Za devatero horami a devatero řekami… Tedy velmi, velmi daleko od malé galské vesnice, kterou tak dobře známe, leží země zvaná Čína. Zde proradný generál, bažící po moci, právě zajal a uvěznil čínskou císařovnu. A protože věhlas nepřemožitelných Galů překvapivě sahá až do Asie, princezna Fu Yi neomylně ví, na koho se obrátit. Se svou věrnou strážkyní Na Ban Čim a synovcem fénického kupce Popkornise prchá ze země. Samozřejmě požádá o pomoc ty nejlepší z nejlepších, udatné válečníky, Asterixe a Obelixe. Naši dva nerozluční hrdinové se s princeznou vydávají na dobrodružnou výpravu za záchranou císařovny i celé Číny…"
                 },
                 new()
                 {
@@ -67,7 +69,8 @@ namespace CinemaApi.Entities.Mock
                         new() { DateTime = new DateTime(2023, 3, 17, 15, 0, 0), Price = 180, Hall = hallOne },
                         new() { DateTime = new DateTime(2023, 3, 18, 17, 0, 0), Price = 180, Hall = hallOne },
                         new() { DateTime = new DateTime(2023, 3, 19, 20, 0, 0), Price = 180, Hall = hallOne },
-                    }
+                    },
+                    Description = "John Wick (Keanu Reeves) odhalí cestu, jak porazit Nejvyšší radu. Než se mu však podaří získat svobodu, musí čelit novému nepříteli, který má mocné spojence po celém světě. Bude to o to težší, že nová spojenectví mění staré přátele v nepřátele..."
                 },
 
                 new()
@@ -86,7 +89,8 @@ namespace CinemaApi.Entities.Mock
                         new() { DateTime = new DateTime(2023, 3, 18, 20, 0, 0), Price = 180, Hall = hallTwo },
                         new() { DateTime = new DateTime(2023, 3, 19, 18, 0, 0), Price = 180, Hall = hallTwo },
                         new() { DateTime = new DateTime(2023, 3, 20, 16, 0, 0), Price = 180, Hall = hallTwo },
-                    }
+                    },
+                    Description = "Pavlovi (Jakub Prachař) sice táhne na čtyřicet, ale stále ještě úplně nedospěl. Bydlí s mámou a chybí mu odvaha dělat v životě to, co by ho skutečně naplňovalo. Tu získá až ve chvíli, kdy potká svou dětskou lásku Terezu (Tereza Ramba) a prožije s ní dobrodružnou noc. Tereza však není připravena opustit svůj dosavadní život pro někoho, kdo podle ní není opravdový chlap. Pavel se tedy rozhodne se přesně takovým chlapem stát. Přihlásí se na výcvikový tábor v Tatrách vedený Weisnerem (Ondřej Sokol), který vyučuje vše od sekání dříví po svádění žen. Podaří se Pavlovi překonat sama sebe a stát se chlapem, po kterém Tereza touží?"
                 }
 
 
